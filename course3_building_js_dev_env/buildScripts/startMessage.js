3fd7958c7c3a0ed36d3a1a84b07c29adf8005a95
a7mad3akef <a7mad3akef@gmail.com>
2017-06-21 18:26:22 +0200

    finished bundling

--- course3_building_js_dev_env/buildScripts/startMessage.js
+++ course3_building_js_dev_env/buildScripts/startMessage.js
@@ -1,5 +1,5 @@
-var chalk = require('chalk');
-
+// var chalk = require('chalk');
+import chalk from 'chalk';
 console.log(chalk.green('starting app in dev mode... '));
 
 
---
+++ course3_building_js_dev_env/src/index.js
@@ -0,0 +1,8 @@
+import numeral from 'numeral';
+import './index.css'
+
+debugger;  
+
+const courseValue = numeral(1000).format('$0,0.00');
+
+console.log(`i would pay ${courseValue} for this course`);
